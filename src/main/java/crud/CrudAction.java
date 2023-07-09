8c3cb4c7b43b425e1a460231702c5cfc67dc5329
Daniel Widdis <widdis@gmail.com>
2023-07-08 19:57:11 -0700

    Implement remaining handlers

--- src/main/java/crud/CrudAction.java
+++ src/main/java/crud/CrudAction.java
@@ -4,17 +4,20 @@ import java.io.IOException;
 import java.util.List;
 import java.util.function.Function;
 
-import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field;
-import org.apache.lucene.document.StringField;
 import org.opensearch.client.opensearch.OpenSearchClient;
 import org.opensearch.client.opensearch._types.OpenSearchException;
+import org.opensearch.client.opensearch._types.Result;
+import org.opensearch.client.opensearch.core.DeleteRequest;
+import org.opensearch.client.opensearch.core.DeleteResponse;
+import org.opensearch.client.opensearch.core.GetRequest;
+import org.opensearch.client.opensearch.core.GetResponse;
 import org.opensearch.client.opensearch.core.IndexRequest;
 import org.opensearch.client.opensearch.core.IndexResponse;
+import org.opensearch.client.opensearch.core.UpdateRequest;
+import org.opensearch.client.opensearch.core.UpdateResponse;
 import org.opensearch.client.opensearch.indices.CreateIndexRequest;
 import org.opensearch.client.opensearch.indices.ExistsRequest;
 import org.opensearch.client.transport.endpoints.BooleanResponse;
-import org.opensearch.extensions.rest.ExtensionRestResponse;
 import org.opensearch.rest.NamedRoute;
 import org.opensearch.rest.RestRequest;
 import org.opensearch.rest.RestRequest.Method;
@@ -36,22 +39,22 @@ public class CrudAction extends BaseExtensionRestHandler {
         return List.of(
             new NamedRoute.Builder().method(Method.PUT)
                 .path("/sample")
-                .uniqueName("extension1:sample/create")
+                .uniqueName("crud_extension:sample/create")
                 .handler(createHandler)
                 .build(),
             new NamedRoute.Builder().method(Method.GET)
                 .path("/sample/{id}")
-                .uniqueName("extension1:sample/get")
+                .uniqueName("crud_extension:sample/get")
                 .handler(readHandler)
                 .build(),
             new NamedRoute.Builder().method(Method.POST)
                 .path("/sample/{id}")
-                .uniqueName("extension1:sample/post")
+                .uniqueName("crud_extension:sample/post")
                 .handler(updateHandler)
                 .build(),
             new NamedRoute.Builder().method(Method.DELETE)
                 .path("/sample/{id}")
-                .uniqueName("extension1:sample/delete")
+                .uniqueName("crud_extension:sample/delete")
                 .handler(deleteHandler)
                 .build()
         );
@@ -60,28 +63,84 @@ public class CrudAction extends BaseExtensionRestHandler {
     Function<RestRequest, RestResponse> createHandler = (request) -> {
         IndexResponse response;
         try {
+            // Create index if it doesn't exist
             BooleanResponse exists = client.indices().exists(new ExistsRequest.Builder().index("crudsample").build());
             if (!exists.value()) {
                 client.indices().create(new CreateIndexRequest.Builder().index("crudsample").build());
             }
-            Document doc = new Document();
-            doc.add(new StringField("field", "value", Field.Store.YES));
-            response = client.index(new IndexRequest.Builder<Document>().index("crudsample").document(doc).build());
+            // Now add our document
+            CrudData crudData = new CrudData();
+            crudData.setValue("value");
+            response = client.index(new IndexRequest.Builder<CrudData>().index("crudsample").document(crudData).build());
         } catch (OpenSearchException | IOException e) {
             return exceptionalRequest(request, e);
         }
-        return createJsonResponse(request, RestStatus.OK, "_id", response.id());
+        if (response.result() == Result.Created) {
+            return createJsonResponse(request, RestStatus.OK, "_id", response.id());
+        }
+        return createJsonResponse(request, RestStatus.INTERNAL_SERVER_ERROR, "failed", response.result().toString());
     };
 
     Function<RestRequest, RestResponse> readHandler = (request) -> {
-        return new ExtensionRestResponse(request, RestStatus.OK, "To be implemented");
+        GetResponse<CrudData> response;
+        // Parse ID from request
+        String id = request.param("id");
+        try {
+            response = client.get(new GetRequest.Builder().index("crudsample").id(id).build(), CrudData.class);
+        } catch (OpenSearchException | IOException e) {
+            return exceptionalRequest(request, e);
+        }
+        if (response.found()) {
+            return createJsonResponse(request, RestStatus.OK, "value", response.source().getValue());
+        }
+        return createJsonResponse(request, RestStatus.NOT_FOUND, "error", "not_found");
     };
 
     Function<RestRequest, RestResponse> updateHandler = (request) -> {
-        return new ExtensionRestResponse(request, RestStatus.OK, "To be implemented");
+        UpdateResponse<CrudData> response;
+        // Parse ID from request
+        String id = request.param("id");
+        // Now create the new document to update with
+        CrudData crudData = new CrudData();
+        crudData.setValue("new value");
+        try {
+            response = client.update(
+                new UpdateRequest.Builder<CrudData, CrudData>().index("crudsample").id(id).doc(crudData).build(),
+                CrudData.class
+            );
+        } catch (OpenSearchException | IOException e) {
+            return exceptionalRequest(request, e);
+        }
+        if (response.result() == Result.Updated) {
+            return createEmptyJsonResponse(request, RestStatus.OK);
+        }
+        return createJsonResponse(request, RestStatus.INTERNAL_SERVER_ERROR, "failed", response.result().toString());
     };
 
     Function<RestRequest, RestResponse> deleteHandler = (request) -> {
-        return new ExtensionRestResponse(request, RestStatus.OK, "To be implemented");
+        DeleteResponse response;
+        // Parse ID from request
+        String id = request.param("id");
+        try {
+            response = client.delete(new DeleteRequest.Builder().index("crudsample").id(id).build());
+        } catch (OpenSearchException | IOException e) {
+            return exceptionalRequest(request, e);
+        }
+        if (response.result() == Result.Deleted) {
+            return createEmptyJsonResponse(request, RestStatus.OK);
+        }
+        return createJsonResponse(request, RestStatus.INTERNAL_SERVER_ERROR, "failed", response.result().toString());
     };
+
+    public static class CrudData {
+        private String value;
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+    }
 }
